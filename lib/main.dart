c26d9a72171a553d74572f568643150ed0f71391
NobodyForNothing <82763757+NobodyForNothing@users.noreply.github.com>
2024-06-08 12:07:49 +0200

    android support

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,65 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'app.dart';
 import 'dev/manager.dart';
+
 void main() async {
-  final manager = _loadManager();
-  manager.addListener(() => _saveManager(manager));
+  WidgetsFlutterBinding.ensureInitialized();
+  final manager = await _loadManager();
+  manager.addListener(() => unawaited(_saveManager(manager)));
 
   runApp(PlugControlApp(
     manager: manager
   ));
 }
 
-Manager _loadManager() {
-  final file = File('devlist');
-  if (file.existsSync()) {
-    try {
-      final data = file.readAsStringSync();
-      return Manager.deserialize(data);
-    } on FileSystemException {
-      try {
-        file.deleteSync();
-      } on FileSystemException {
-        // Do nothing
-      }
-    }
+Future<Manager> _loadManager() async {
+  final File? file = await _getSaveFile();
+  if (file == null) return Manager();
+
+  String fileContent;
+  try {
+    fileContent = file.readAsStringSync();
+  } on FileSystemException {
+    print("Couldn't read saved plug ips.");
+    return Manager();
   }
-  return Manager();
+
+  try {
+    return Manager.deserialize(fileContent);
+  } catch (e) {
+    print("Failed decoding saved ips.");
+    file.deleteSync();
+    return Manager();
+  }
 }
 
-void _saveManager(Manager manager) {
+Future<void> _saveManager(Manager manager) async {
   try {
-    File('devlist').writeAsStringSync(manager.serialize());
+    final File? file = await _getSaveFile();
+    if (file == null) {
+      return;
+    }
+    file.writeAsStringSync(manager.serialize());
   } on FileSystemException {
     print('Persisting data failed.');
   }
+}
+
+Future<File?> _getSaveFile() async {
+  Directory dir;
+  try {
+    dir = await getApplicationDocumentsDirectory();
+  } on MissingPlatformDirectoryException {
+    print('No application documents dir.');
+    return null;
+  }
+
+  final filePath = join(dir.path, 'plgs.ips');
+  return File(filePath);
 }
